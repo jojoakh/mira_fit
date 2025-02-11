ec1a61bea425e6af9cd34116d13c9b6ad0d29551
jojoakh <akhigbejoy5@gmail.com>
2025-02-11 18:35:33 +0100

    Prepare for deployment

--- home/views.py
+++ home/views.py
@@ -1,14 +1,7 @@
-from django.shortcuts import render, redirect
-from .forms import NewsletterForm
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    form = NewsletterForm()  # Initialize the form
-    if request.method == "POST":
-        form = NewsletterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    return render(request, 'home/home.html', {'form': form})
+    return render(request, 'home/home.html')
