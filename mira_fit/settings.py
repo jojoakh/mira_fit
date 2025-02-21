298883ab1ddb1712d8d9ac86e98ec732d29ffa01
jojoakh <akhigbejoy5@gmail.com>
2025-02-21 01:30:50 +0100

    Created contact app and added urls.py to contact app

--- mira_fit/settings.py
+++ mira_fit/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'contact',
 
     # Other
     'storages',
--- mira_fit/urls.py
+++ mira_fit/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),  # Allauth URLs
+    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
