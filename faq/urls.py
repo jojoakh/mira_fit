70f94c4d1ea049a8a017ae07b5005f1d60853709
jojoakh <akhigbejoy5@gmail.com>
2025-02-23 22:42:22 +0100

    Remove faq models and update templates

--- faq/urls.py
+++ faq/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import faq_list
+from .views import faq
 
 urlpatterns = [
-    path("", faq_list, name="faq_list"),
+    path('', faq, name='faq')
 ]
--- faq/views.py
+++ faq/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from .models import FAQ
 
 # Create your views here.
 
 
-def faq_list(request):
-    faqs = FAQ.objects.all().order_by('-created_at')
-    return render(request, "faq/faq_list.html", {"faqs": faqs})
+def faq(request):
+
+    return render(request, "faq/faq_list.html")
