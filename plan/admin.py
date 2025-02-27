fceff7f8a51bacddfa5c072c8d8712e63a6760de
jojoakh <akhigbejoy5@gmail.com>
2025-02-27 22:44:03 +0100

    Enhanced the plan admin with editable field

--- plan/admin.py
+++ plan/admin.py
@@ -3,4 +3,14 @@ from .models import Plan
 
 # Register your models here.
 
-admin.site.register(Plan)
+
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'duration', 'price')
+    list_editable = ('duration', 'price')
+    search_fields = ('name',)
+    list_filter = ('duration',)
+    fields = ('name', 'description', 'duration', 'price')
+    readonly_fields = ()
+
+
+admin.site.register(Plan, PlanAdmin)
